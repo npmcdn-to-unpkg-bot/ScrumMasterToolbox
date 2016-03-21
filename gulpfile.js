374d844753d0b4fb0934ee4ca3f35908e218326f
Katarzyna Pietraszko <k.pietraszko@centrala.kaczmarski.pl>
2016-03-21 12:07:40 +0100

    rebuilt gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
-//sass configuration
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var gutil = require('gulp-util');
+var notify = require('gulp-notify');
+var watch = require('gulp-watch');
 
-gulp.task('sass', function(){
-    gulp.src('*.scss') //take set of changed SASS files and run them
+gulp.task('styles', function() {
+    gulp.src('Content/styles/*.scss') //take set of changed SASS files and run them
         .pipe(sass()) //through compiler (gulp-sass)
-        .pipe(gulp.dest(function(f){
-            return f.base;
+        .pipe(gulp.dest('Content/styles/'))
+        .pipe(notify({
+            message: "You just got super Sassy!"
         }))
 });
 
-gulp.task('default', function(){
-    gulp.watch('*.scss', ['sass']); //watch for any changes to any SASS files in current folder
+gulp.task('default', function() {
+    gulp.watch('Content/styles/*.scss', ['styles']);
 })
+
+/*gulp.task('watch', function() {
+    //watch scss files
+    gulp.watch('Content/styles/*.scss', function() {
+        gulp.run('styles');
+    })
+})
+
+gulp.task('default', ['styles', 'watch']);*/
